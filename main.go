bbc3a50e87a5cf5278791290a561bfea8e22333d
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2026-02-16 02:35:25 +0330

    main.go

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/4n0nymou3/CF-Clean-IP-Scanner/config"
+	"github.com/4n0nymou3/CF-Clean-IP-Scanner/scanner"
+	"github.com/4n0nymou3/CF-Clean-IP-Scanner/utils"
+)
+
+const version = "1.0.0"
+
+func main() {
+	utils.PrintHeader()
+	
+	green := color.New(color.FgGreen, color.Bold)
+	green.Println("\nDesigned by: Anonymous\n")
+	
+	cyan := color.New(color.FgCyan)
+	cyan.Printf("Version: %s\n", version)
+	cyan.Println("Starting Cloudflare Clean IP Scanner...\n")
+	
+	time.Sleep(1 * time.Second)
+	
+	ipRanges := config.GetCloudflareRanges()
+	
+	yellow := color.New(color.FgYellow)
+	yellow.Printf("IP Ranges: %d\n", len(ipRanges))
+	yellow.Println("Generating random IPs...\n")
+	
+	ips := scanner.GenerateIPs(ipRanges, 200)
+	
+	cyan.Printf("Total IPs to scan: %d\n\n", len(ips))
+	
+	results := scanner.ScanIPs(ips)
+	
+	if len(results) == 0 {
+		red := color.New(color.FgRed, color.Bold)
+		red.Println("\nNo clean IPs found!")
+		red.Println("Please try again later.")
+		os.Exit(1)
+	}
+	
+	topResults := results
+	if len(results) > 10 {
+		topResults = results[:10]
+	}
+	
+	utils.PrintResults(topResults)
+	
+	err := utils.SaveResults(results, "clean_ips.txt")
+	if err != nil {
+		red := color.New(color.FgRed)
+		red.Printf("\nError saving file: %v\n", err)
+	} else {
+		green.Println("\nResults saved to clean_ips.txt successfully!")
+		green.Printf("Total IPs found: %d\n", len(results))
+	}
+	
+	fmt.Println()
+	cyan := color.New(color.FgCyan, color.Bold)
+	cyan.Println("========================================")
+	cyan.Println("     Scan completed successfully!")
+	cyan.Println("========================================")
+	fmt.Println()
+}
