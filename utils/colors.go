8ea45b3ed9e7f2ce59c5d63f84a35d3adc64c3c2
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2026-02-16 07:39:13 +0330

    colors.go

--- utils/colors.go
+++ utils/colors.go
@@ -8,12 +8,14 @@ import (
 
 func PrintHeader() {
 	cyan := color.New(color.FgCyan, color.Bold)
+	cyan.Println("========================================")
+	cyan.Println("   CLOUDFLARE CLEAN IP SCANNER")
+	cyan.Println("   Find the fastest Cloudflare IPs")
+	cyan.Println("========================================")
+}
+
+func PrintDesigner() {
 	green := color.New(color.FgGreen, color.Bold)
-	yellow := color.New(color.FgYellow)
-	
+	green.Println("...:..::.::: Designed by: Anonymous :::.::..:...")
 	fmt.Println()
-	cyan.Println("========================================")
-	green.Println("   CLOUDFLARE CLEAN IP SCANNER")
-	yellow.Println("   Find the fastest Cloudflare IPs")
-	cyan.Println("========================================")
 }
