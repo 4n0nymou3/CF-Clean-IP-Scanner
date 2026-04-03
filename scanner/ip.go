4cc1da688e0c1da3b581ca136fc10727271de086
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2026-04-04 00:31:00 +0330

    ip.go

--- scanner/ip.go
+++ scanner/ip.go
@@ -4,127 +4,63 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
-	"strconv"
 	"strings"
 )
 
-func randIPEndWith(num byte) byte {
-	if num == 0 {
-		return 0
-	}
-	return byte(rand.Intn(int(num)))
-}
-
 type IPRanges struct {
-	ips     []*net.IPAddr
-	mask    string
-	firstIP net.IP
-	ipNet   *net.IPNet
+	ips  []*net.IPAddr
+	seen map[string]bool
 }
 
 func newIPRanges() *IPRanges {
 	return &IPRanges{
-		ips: make([]*net.IPAddr, 0),
+		ips:  make([]*net.IPAddr, 0),
+		seen: make(map[string]bool),
 	}
-}
-
-func (r *IPRanges) fixIP(ip string) string {
-	if i := strings.IndexByte(ip, '/'); i < 0 {
-		if isIPv4(ip) {
-			r.mask = "/32"
-		} else {
-			r.mask = "/128"
-		}
-		ip += r.mask
-	} else {
-		r.mask = ip[i:]
-	}
-	return ip
-}
-
-func (r *IPRanges) parseCIDR(ip string) {
-	var err error
-	if r.firstIP, r.ipNet, err = net.ParseCIDR(r.fixIP(ip)); err != nil {
-		fmt.Printf("ParseCIDR error: %v\n", err)
-		return
-	}
-}
-
-func (r *IPRanges) appendIPv4(d byte) {
-	r.appendIP(net.IPv4(r.firstIP[12], r.firstIP[13], r.firstIP[14], d))
 }
 
 func (r *IPRanges) appendIP(ip net.IP) {
 	r.ips = append(r.ips, &net.IPAddr{IP: ip})
 }
 
-func (r *IPRanges) getIPRange() (minIP, hosts byte) {
-	minIP = r.firstIP[15] & r.ipNet.Mask[3]
-	m := net.IPv4Mask(255, 255, 255, 255)
-	for i, v := range r.ipNet.Mask {
-		m[i] ^= v
-	}
-	total, _ := strconv.ParseInt(m.String(), 16, 32)
-	if total > 255 {
-		hosts = 255
+func (r *IPRanges) expandCIDR(cidr string) {
+	cidr = strings.TrimSpace(cidr)
+	if cidr == "" {
 		return
 	}
-	hosts = byte(total)
-	return
-}
 
-func (r *IPRanges) chooseIPv4() {
-	if r.mask == "/32" {
-		r.appendIP(r.firstIP)
-	} else {
-		minIP, hosts := r.getIPRange()
-		maxIterations := 0
-		for r.ipNet.Contains(r.firstIP) {
-			samplesPerRow := 1
-			if hosts == 255 {
-				samplesPerRow = 16
-			}
-			for i := 0; i < samplesPerRow; i++ {
-				r.appendIPv4(minIP + randIPEndWith(hosts))
-			}
-			r.firstIP[14]++
-			if r.firstIP[14] == 0 {
-				r.firstIP[13]++
-				if r.firstIP[13] == 0 {
-					r.firstIP[12]++
-				}
-			}
-			maxIterations++
-			if maxIterations > 10000 {
-				break
-			}
-		}
+	_, ipNet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		fmt.Printf("ParseCIDR error for %s: %v\n", cidr, err)
+		return
+	}
+
+	networkKey := ipNet.String()
+	if r.seen[networkKey] {
+		return
+	}
+	r.seen[networkKey] = true
+
+	ip := cloneIP(ipNet.IP)
+	for ipNet.Contains(ip) {
+		clone := make(net.IP, len(ip))
+		copy(clone, ip)
+		r.appendIP(clone)
+		incrementIP(ip)
 	}
 }
 
-func (r *IPRanges) chooseIPv6() {
-	if r.mask == "/128" {
-		r.appendIP(r.firstIP)
-	} else {
-		var tempIP uint8
-		maxIterations := 0
-		for r.ipNet.Contains(r.firstIP) {
-			r.firstIP[15] = randIPEndWith(255)
-			r.firstIP[14] = randIPEndWith(255)
-			targetIP := make([]byte, len(r.firstIP))
-			copy(targetIP, r.firstIP)
-			r.appendIP(targetIP)
-			for i := 13; i >= 0; i-- {
-				tempIP = r.firstIP[i]
-				r.firstIP[i] += randIPEndWith(255)
-				if r.firstIP[i] >= tempIP {
-					break
-				}
-			}
-			maxIterations++
-			if maxIterations > 10000 {
-				break
-			}
+func cloneIP(ip net.IP) net.IP {
+	clone := make(net.IP, len(ip))
+	copy(clone, ip)
+	return clone
+}
+
+func incrementIP(ip net.IP) {
+	for i := len(ip) - 1; i >= 0; i-- {
+		ip[i]++
+		if ip[i] != 0 {
+			break
 		}
 	}
 }
@@ -135,17 +71,24 @@ func isIPv4(ip string) bool {
 
 func GenerateIPs(ranges []string) []*net.IPAddr {
 	ipRanges := newIPRanges()
-	for _, ipRange := range ranges {
-		ipRange = strings.TrimSpace(ipRange)
-		if ipRange == "" {
+	for _, r := range ranges {
+		r = strings.TrimSpace(r)
+		if r == "" {
 			continue
 		}
-		ipRanges.parseCIDR(ipRange)
-		if isIPv4(ipRange) {
-			ipRanges.chooseIPv4()
-		} else {
-			ipRanges.chooseIPv6()
+		if !strings.Contains(r, "/") {
+			if isIPv4(r) {
+				r += "/32"
+			} else {
+				r += "/128"
+			}
 		}
+		ipRanges.expandCIDR(r)
 	}
+
+	rand.Shuffle(len(ipRanges.ips), func(i, j int) {
+		ipRanges.ips[i], ipRanges.ips[j] = ipRanges.ips[j], ipRanges.ips[i]
+	})
+
 	return ipRanges.ips
 }
